fcd80261b8c0d64fcbff4c18d72aa5aaa50b3718
Victor di Ruggiero <victor@diruggiero.fr>
2018-12-13 16:41:22 +0100

    Data Science - Playing with pandas

--- pandas_playground.py
+++ pandas_playground.py
@@ -16,7 +16,7 @@ http://www.gregreda.com/2013/10/26/intro-to-pandas-data-structures/
 # Change False to True to create a Series object
 if False:
     series = pd.Series(['Dave', 'Cheng-Han', 'Udacity', 42, -1789710578])
-    print series
+    print(series)
 
 '''
 You can also manually assign indices to the items in the Series when
@@ -28,7 +28,7 @@ if False:
     series = pd.Series(['Dave', 'Cheng-Han', 359, 9001],
                        index=['Instructor', 'Curriculum Manager',
                               'Course Number', 'Power Level'])
-    print series
+    print(series)
 
 '''
 You can use index to select specific items from the Series
@@ -38,17 +38,17 @@ if False:
     series = pd.Series(['Dave', 'Cheng-Han', 359, 9001],
                        index=['Instructor', 'Curriculum Manager',
                               'Course Number', 'Power Level'])
-    print series['Instructor']
-    print ""
-    print series[['Instructor', 'Curriculum Manager', 'Course Number']]
+    print(series['Instructor'])
+    print("")
+    print(series[['Instructor', 'Curriculum Manager', 'Course Number']])
 
 '''
 You can also use boolean operators to select specific items from the Series
 '''
 # Change False to True to see boolean indexing in action
-if False:
+if True:
     cuteness = pd.Series([1, 2, 3, 4, 5], index=['Cockroach', 'Fish', 'Mini Pig',
                                                  'Puppy', 'Kitten'])
-    print cuteness > 3
-    print ""
-    print cuteness[cuteness > 3]
+    print(cuteness > 3)
+    print("")
+    print(cuteness[cuteness > 3])
