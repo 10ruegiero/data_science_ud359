5508e6aecdab91bc4d901c4ef0078b556f9cd983
Victor di Ruggiero <victor@diruggiero.fr>
2018-12-13 16:44:43 +0100

    Data Science - Playing with pandas dataframe

--- pandas_playground_dataframe.py
+++ pandas_playground_dataframe.py
@@ -18,14 +18,14 @@ constructor:
 2) The associating list will be the values within that column.
 '''
 # Change False to True to see Dataframes in action
-if False:
+if True:
     data = {'year': [2010, 2011, 2012, 2011, 2012, 2010, 2011, 2012],
             'team': ['Bears', 'Bears', 'Bears', 'Packers', 'Packers', 'Lions',
                      'Lions', 'Lions'],
             'wins': [11, 8, 10, 15, 11, 6, 10, 4],
             'losses': [5, 8, 6, 1, 5, 10, 6, 12]}
     football = pd.DataFrame(data)
-    print football
+    print(football)
 
 '''
 Pandas also has various functions that will help you understand some basic
@@ -44,10 +44,10 @@ if False:
             'wins': [11, 8, 10, 15, 11, 6, 10, 4],
             'losses': [5, 8, 6, 1, 5, 10, 6, 12]}
     football = pd.DataFrame(data)
-    print football.dtypes
-    print ""
-    print football.describe()
-    print ""
-    print football.head()
-    print ""
-    print football.tail()
+    print(football.dtypes)
+    print("")
+    print(football.describe())
+    print("")
+    print(football.head())
+    print("")
+    print(football.tail())
