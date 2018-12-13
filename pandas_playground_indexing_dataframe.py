6c89a88853fa8ba657506046aae8e07a2fd9e87c
Victor di Ruggiero <victor@diruggiero.fr>
2018-12-13 16:58:09 +0100

    Data Science - Indexing dataframe

--- pandas_playground_indexing_dataframe.py
+++ pandas_playground_indexing_dataframe.py
@@ -20,11 +20,11 @@ if False:
             'wins': [11, 8, 10, 15, 11, 6, 10, 4],
             'losses': [5, 8, 6, 1, 5, 10, 6, 12]}
     football = pd.DataFrame(data)
-    print football['year']
-    print ''
-    print football.year  # shorthand for football['year']
-    print ''
-    print football[['year', 'wins', 'losses']]
+    print(football['year'])
+    print('')
+    print(football.year)  # shorthand for football['year']
+    print('')
+    print(football[['year', 'wins', 'losses']])
 
 '''
 Row selection can be done through multiple ways.
@@ -38,19 +38,19 @@ You can also combine multiple selection requirements through boolean
 operators like & (and) or | (or)
 '''
 # Change False to True to see boolean indexing in action
-if False:
+if True:
     data = {'year': [2010, 2011, 2012, 2011, 2012, 2010, 2011, 2012],
             'team': ['Bears', 'Bears', 'Bears', 'Packers', 'Packers', 'Lions',
                      'Lions', 'Lions'],
             'wins': [11, 8, 10, 15, 11, 6, 10, 4],
             'losses': [5, 8, 6, 1, 5, 10, 6, 12]}
     football = pd.DataFrame(data)
-    print football.iloc[[0]]
-    print ""
-    print football.loc[[0]]
-    print ""
-    print football[3:5]
-    print ""
-    print football[football.wins > 10]
-    print ""
-    print football[(football.wins > 10) & (football.team == "Packers")]
+    print(football.iloc[[0]])
+    print("")
+    print(football.loc[[0]])
+    print("")
+    print(football[3:5])
+    print("")
+    print(football[football.wins > 10])
+    print("")
+    print(football[(football.wins > 10) & (football.team == "Packers")])
